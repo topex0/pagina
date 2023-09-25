f9b9c3fd642b7c31476d08578041ae3746bbb4e8
ᴛᴘx <115488489+topex0@users.noreply.github.com>
2023-09-25 18:01:48 +0200

    Add files via upload
    Signed-off-by: ᴛᴘx <115488489+topex0@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvXZLrS8vs6oZYD7P70CJnYPZqgwR1Ouo",
+  authDomain: "tpxtopexx.firebaseapp.com",
+  projectId: "tpxtopexx",
+  storageBucket: "tpxtopexx.appspot.com",
+  messagingSenderId: "121382262084",
+  appId: "1:121382262084:web:2c5fb6ab5a3cc402c1b311",
+  measurementId: "G-65F4CW3ZRM"
+};
+exportaciones.solicitud = funciones.https.onRequest((solicitud, respuesta) => { 
+  // ... 
+});
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
