3ccd8227fb6ca20f50d44feab5668fc326c7724c
ᴛᴘx <115488489+topex0@users.noreply.github.com>
2023-09-25 18:13:19 +0200

    Add files via upload
    Signed-off-by: ᴛᴘx <115488489+topex0@users.noreply.github.com>

---
+++ efecto.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuElement = document.querySelector('.desktop1-barralateral-menu');
+    menuElement.classList.add('animate-slide');
+  });
+  
